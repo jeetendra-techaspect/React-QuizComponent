7a1328ec4e4d6794ac29b647761d1d34d9060a23
jeetendra.s <jeetendra.s@techaspect.com>
2019-02-20 02:08:55 +0530

    add reset functionality

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
         }
     }
 
+    handleResetClick() {
+        this.setState({
+            quiz_position : 1
+        })
+    }
+
     showNextQuestion() {
         this.setState({
             quiz_position : this.state.quiz_position + 1
@@ -21,7 +27,9 @@ class Quiz extends Component {
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return <div>
-            {isQuizEnd ? <QuizEnd/> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+            {isQuizEnd ? 
+            <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : 
+            <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
             
         </div>
     }
